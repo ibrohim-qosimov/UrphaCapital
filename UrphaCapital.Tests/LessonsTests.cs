f6abf9591463acf8d9fef0714732abddff3c95da
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-08-03 14:01:52 +0500

    add tests

---
+++ UrphaCapital.Tests/LessonsTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using UrphaCapital.Application.Abstractions;
+using UrphaCapital.Application.UseCases.Lessons.Commands;
+using UrphaCapital.Application.UseCases.Lessons.Handlers.CommandHandlers;
+
+namespace UrphaCapital.Tests
+{
+    public partial class LessonsTests
+    {
+        private readonly Mock<IApplicationDbContext> _mockContext;
+        private readonly DeleteLessonCommandHandler _deleteLessonHandler;
+
+        public LessonsTests()
+        {
+            _mockContext = new Mock<IApplicationDbContext>();
+            _deleteLessonHandler = new DeleteLessonCommandHandler(_mockContext.Object);
+        }
+    }
+}
