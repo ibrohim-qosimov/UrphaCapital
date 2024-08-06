0c2028aa13af800e9e0a0454eb6cb81a0fe408e7
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-08-06 21:03:15 +0500

    course crud partially done

---
+++ UrphaCapital.Application/UseCases/Courses/Handlers/CommandHandlers/CreateCourseCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrphaCapital.Application.UseCases.Courses.Commands;
+using UrphaCapital.Application.ViewModels;
+
+namespace UrphaCapital.Application.UseCases.Courses.Handlers.CommandHandlers
+{
+    public class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, ResponseModel>
+    {
+    }
+}
---
+++ UrphaCapital.Application/UseCases/Courses/Handlers/QueryHandlers/GetCourseByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrphaCapital.Application.Abstractions;
+using UrphaCapital.Application.UseCases.Courses.Queries;
+using UrphaCapital.Domain.Entities;
+
+namespace UrphaCapital.Application.UseCases.Courses.Handlers.QueryHandlers
+{
+    public class GetCourseByIdQueryHandler : IRequestHandler<GetCourseByIdQuery, Course>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetCourseByIdQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Course> Handle(GetCourseByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Courses.FirstOrDefaultAsync(x => x.Id == request.Id) ??
+                 throw new Exception("Not found");
+        }
+    }
+}
