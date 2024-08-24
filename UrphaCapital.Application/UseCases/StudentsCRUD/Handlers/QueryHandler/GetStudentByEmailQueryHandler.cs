c7a0de2adfbf437975d02a32dd68ebde0f964009
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-08-24 13:32:18 +0500

    studnet login completed

---
+++ UrphaCapital.Application/UseCases/StudentsCRUD/Handlers/QueryHandler/GetStudentByEmailQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrphaCapital.Application.Abstractions;
+using UrphaCapital.Application.UseCases.StudentsCRUD.Queries;
+using UrphaCapital.Domain.Entities.Auth;
+
+namespace UrphaCapital.Application.UseCases.StudentsCRUD.Handlers.QueryHandler
+{
+    public class GetStudentByEmailQueryHandler : IRequestHandler<GetStudentByEmailQuery, Student>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetStudentByEmailQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Student> Handle(GetStudentByEmailQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Students
+                .FirstOrDefaultAsync(x => x.Email == request.Email, cancellationToken)
+                    ?? throw new Exception("Not found");
+        }
+    }
+}
