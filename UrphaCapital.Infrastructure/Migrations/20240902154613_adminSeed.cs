0b6ebab1e4b43964e8fd4417bf6dab6c41d15f60
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-09-02 20:46:23 +0500

    aytkim kelar

---
+++ UrphaCapital.Infrastructure/Migrations/20240902154613_adminSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrphaCapital.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class adminSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Admins",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "PasswordHash", "Salt" },
+                values: new object[] { "WNnk4s2M362PsRP5CEEW8rcEZj8oDrzMcAUzj33CV+w=", "e253cb64-696a-4acd-b812-ca1bb32d371b" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Admins",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "PasswordHash", "Salt" },
+                values: new object[] { "Admin01!", "b05c0da6-5080-4bfc-80e7-f01917f47849" });
+        }
+    }
+}
